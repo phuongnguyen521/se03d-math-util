33b4415b842c1b9ee90139401efd7e151c390ed3
phuongnguyen521 <ngcphungnguyn@gmail.com>
2021-03-24 10:09:11 +0700

    Add JUnit & RED ALERT

--- src/com/phuong/mathutil/MathUtility.java
+++ src/com/phuong/mathutil/MathUtility.java
@@ -31,7 +31,7 @@ public class MathUtility {
        if (n == 0 || n == 1){
            return 1;
        }
-       long result = 0;
+       long result = 1;
         for (int i = 2; i <= n; i++) {
             result *= i;
         }
